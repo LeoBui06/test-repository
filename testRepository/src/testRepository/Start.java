e7c2e0fe40bc35a9b18606069e907b463fd8d865
LeoBui06 <s8901804e@majorana-giorgi.edu.it>
2024-11-25 09:36:49 +0100

    mod appunti

--- testRepository/src/testRepository/Start.java
+++ testRepository/src/testRepository/Start.java
@@ -3,6 +3,7 @@ package testRepository;
 public class Start {
 
 	public static void main(String args) {
+		//quando fai commit change il commit message è molto molto MOLTO importante
 		System.out.pritln("ciao ciri!");
 	}
 }
